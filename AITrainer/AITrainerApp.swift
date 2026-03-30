864bc72b5fe41ca42eac4bbd9cf23bf4e984c092
Akshar Pandia <apandia10@gmail.com>
2026-03-30 10:50:27 -0600

    Initial Commit

---
+++ AITrainer/AITrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITrainerApp.swift
+//  AITrainer
+//
+//  Created by Akshar Pandia on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct AITrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
